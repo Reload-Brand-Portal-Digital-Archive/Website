74cf7a2615e441e010cbee89793ee2bf48037e7f
Rizal Ahmad Saepulloh <rizalahmadsaepulloh@gmail.com>
2026-05-01 21:29:11 +0700

    add file upload For Map Report

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ require('./config/database');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const authRoutes = require('./routes/authRoutes');
 const collectionRoutes = require('./routes/collectionRoutes');
